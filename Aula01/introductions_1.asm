aeb7c428e21c3131349d3b81b051d24c857b5391
andre <andreassembly13@gmail.com>
2023-06-01 21:11:21 -0300

    update course

---
+++ Aula01/introductions_1.asm
@@ -0,0 +1,10 @@
+section .data ; -- Normalmente colocamos todas as sections [constantes]
+section .bss ;  -- Colocamos todas as nossas variaveis, coisas que vamos modificar
+section .text ; -- começa o nosso programa!
+global _start ; -- Este Label obrigatoriamente deve ter!
+
+_start:
+       mov EAX, 0x1    ; --  SO estou terminando o programa
+       mov EBX, 0x0    ; --  SO o valor de retorno e 0
+       int 0x80        ; --  
+           
